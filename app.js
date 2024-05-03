c552e9191e42e1b29237005a8737af76ca84c6eb
yasmeenrahimhuda <155715580+yasmeenrahimhuda@users.noreply.github.com>
2024-05-03 23:35:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+// Q1 Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+
+var age = "25 year old"; 
+
+alert("i am " + age);
+
+// Q2 Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+
+
+
+// Q3 Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
+var birthYear = 1990;
+document.write("My birth year is" + birthYear);
+
+// Q4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+
+var visitorName = "John Doe";
+var productTitle = "T-shirt";
+var quantity = 5;
+document.write(visitorName  + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.");
